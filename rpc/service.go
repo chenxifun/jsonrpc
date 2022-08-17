db918895d16204d63979bad0269a1033d1767b0f
chenxifun <me@gaochenxi.com>
2022-08-17 10:50:00 +0800

    method handler crashed add log

--- rpc/service.go
+++ rpc/service.go
@@ -217,7 +217,7 @@ func (c *callback) call(ctx context.Context, method string, args []reflect.Value
 			const size = 64 << 10
 			buf := make([]byte, size)
 			buf = buf[:runtime.Stack(buf, false)]
-			//log.Error("RPC method " + method + " crashed: " + fmt.Sprintf("%v\n%s", err, buf))
+			log.DefLogger().Error("RPC method " + method + " crashed: " + fmt.Sprintf("%v\n%s", err, buf))
 			errRes = errors.New("method handler crashed")
 		}
 	}()
